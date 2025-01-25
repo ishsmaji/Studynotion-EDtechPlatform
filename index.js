a3db38b70a7c379c03f4e835d1cc1aca8fcb9c7c
Isha Maji <116086211+ishsmaji@users.noreply.github.com>
2025-01-25 16:45:33 +0530

    Modified CORS url

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "https://studynotion-e-dtech-platform.vercel.app/",
+        origin: "https://studynotion-e-dtech-platform.vercel.app",
         credentials:true,
     })
 );
