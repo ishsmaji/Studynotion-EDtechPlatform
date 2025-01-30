480c57aa81f5a3a48649e9bb3711a6fe500a9c25
Isha Maji <116086211+ishsmaji@users.noreply.github.com>
2025-01-30 19:17:53 +0530

    Update VerifyEmail.jsx

--- src/Pages/VerifyEmail.jsx
+++ src/Pages/VerifyEmail.jsx
@@ -49,10 +49,20 @@ const VerifyEmail = () => {
                             onChange={setOtp}
                             numInputs={6}
                             renderSeparator={<span className='text-richblack-5' >-</span>}
-                            renderInput={(props) => <input {...props} 
-                            placeholder='-'
-                            className='bg-richblack-800  text-richblack-5 mx-6 rounded-lg p-1'
-                            />}
+                            renderInput={(props) => (
+                                <input
+                                  {...props}
+                                  placeholder="-"
+                                  style={{
+                                    boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+                                  }}
+                                  className="w-[48px] lg:w-[60px] border-0 bg-richblack-800 rounded-[0.5rem] text-richblack-5 aspect-square text-center focus:border-0 focus:outline-2 focus:outline-yellow-50"
+                                />
+                              )}
+                              containerStyle={{
+                                justifyContent: "space-between",
+                                gap: "0 6px",
+                              }}
                             
                         />
                     </div>
